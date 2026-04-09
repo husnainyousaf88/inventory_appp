549b9d9466914a70aab131c4c2b04c04e69e27fd
husnainyousaf88 <husnain.yousaf8888@gmail.com>
2026-04-09 23:06:48 +0200

    test endpoint

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -4,6 +4,11 @@ from app.services.auth_service import register_user, login_user
 
 auth_bp = Blueprint("auth", __name__)
 
+
+@auth_bp.route("/home", methods=["get"])
+def home():
+    return "Flask app is running"
+
 @auth_bp.route("/register", methods=["POST"])
 def register():
     data = request.get_json()

5c6728213a9ded7d644e5fb1a31ce827847dcb0e
husnainyousaf88 <husnain.yousaf8888@gmail.com>
2026-04-09 23:16:01 +0200

    udpated env

--- app/config.py
+++ app/config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL"
+        "DATABASE_URL", "postgres://postgres:newpassword@localhost/inventory_db"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
